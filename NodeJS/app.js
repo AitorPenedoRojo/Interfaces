d8dcd2b55e437b9220053dffa2af0fd4b7273294
Aitor Penedo <aitorpenedo97@gmail.com>
2022-03-29 10:05:37 +0200

    node2

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static(__dirname + '/public'))
+
+
 console.log("hello world")
 
 app.get('/',(req,res)=>{
@@ -13,6 +16,35 @@ app.get('/fecha',(req,res)=>{
 
     res.send('La fecha es '+fecha)
 
+})
+
+app.get('/lista',(req,res)=>{
+
+    res.send(`
+    <html>
+        <body>
+            <ul>
+                <li>Pera</li>
+                <li>Limon</li>
+                <li>Manzana</li>
+            </ul>
+        </body>
+    </html>
+    `)
+})
+
+app.get('/dameJson',(req,res)=>{
+    
+    let objetoAlumno={
+        nombre:'Aria',
+        edad:'24'
+    }
+
+    res.json(objetoAlumno)
+})
+
+app.get('/formulario',(req,res)=>{
+    res.sendFile(__dirname+'/views/index.html')
 })
 
 module.exports = app
